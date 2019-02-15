bf2627f70b5747f29b8b6b86ddc625ce63fa878a
osanaikoutarou <osanaikoutarou+github@gmail.com>
2019-02-15 14:54:31 +0900

    episode

---
+++ ReeachA/Screen/ChannelDetail/ChannelDetailEpisode/ChannelDetailEpisodeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChannelDetailEpisodeTableViewCell.swift
+//  ReeachA
+//
+//  Created by 長内幸太郎 on 2019/02/15.
+//  Copyright © 2019年 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class ChannelDetailEpisodeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ReeachA/Screen/ChannelDetail/ChannelDetailEpisode/ChannelDetailEpisodeViewController.swift
+++ ReeachA/Screen/ChannelDetail/ChannelDetailEpisode/ChannelDetailEpisodeViewController.swift
@@ -10,21 +10,56 @@ import UIKit
 
 class ChannelDetailEpisodeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    var openCell:[IndexPath:Bool] = [:]
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func isOpen(indexPath: IndexPath) -> Bool {
+        if let isOpen = openCell[indexPath] {
+            return isOpen
+        }
+        return false
     }
-    */
+    func toggleOpen(indexPath: IndexPath) -> Bool {
+        let current = isOpen(indexPath: indexPath)
+        let result = !current
+        openCell[indexPath] = result
+        return result
+    }
+}
 
+extension ChannelDetailEpisodeViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if isOpen(indexPath: indexPath) {
+            return 280.0
+        }
+        else {
+            return 140.0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChannelDetailEpisodeTableViewCell", for: indexPath) as! ChannelDetailEpisodeTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        _ = toggleOpen(indexPath: indexPath)
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    
 }
