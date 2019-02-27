ffad6b74b0e5f93d13d9e8de751a727fb9f25168
osanai <osanai@CR-040.local>
2019-02-27 13:05:56 +0900

    post

---
+++ ReeachA/Screen/ChannelDetail/ChannelDetailPost/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  ReeachA
+//
+//  Created by osanai on 2019/02/27.
+//  Copyright © 2019年 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
