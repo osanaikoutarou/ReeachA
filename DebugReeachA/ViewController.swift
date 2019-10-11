cc9c77a21cff396fc16bc183b2348c7c11e5a9a2
Koutarou Osanai <osanaikoutarou@gmail.com>
2019-10-11 16:51:04 +0900

    テスト用ターゲットを追加してみた

---
+++ DebugReeachA/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DebugReeachA
+//
+//  Created by osanai on 2019/10/11.
+//  Copyright © 2019 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var l: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        stackView.removeAll()
+        
+    }
+
+
+}
+
