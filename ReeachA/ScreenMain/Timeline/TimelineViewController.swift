d7af53dfdbaae24d51c3c8c94441340fec679f99
osanaikoutarou <osanaikoutarou+github@gmail.com>
2019-10-17 14:02:04 +0900

    top

--- ReeachA/ScreenMain/Timeline/TimelineViewController.swift
+++ ReeachA/ScreenMain/Timeline/TimelineViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class TimelineViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.register(cellType: ChannelDetailTimelineTableViewCell.self)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +22,16 @@ class TimelineViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TimelineViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
-    */
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(with: ChannelDetailTimelineTableViewCell.self, for: indexPath)
+//        cell.channel = channel
+        return cell
+    }
 }
