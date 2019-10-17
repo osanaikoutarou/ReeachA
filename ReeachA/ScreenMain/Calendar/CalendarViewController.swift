207c629a072d56f7fe78f5c447b4868fccb5fc46
Koutarou Osanai <osanaikoutarou@gmail.com>
2019-10-17 14:34:08 +0900

    top

--- ReeachA/ScreenMain/Calendar/CalendarViewController.swift
+++ ReeachA/ScreenMain/Calendar/CalendarViewController.swift
@@ -13,6 +13,7 @@ class CalendarViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationController?.setNavigationBarHidden(true, animated: false)
         // Do any additional setup after loading the view.
     }
     
